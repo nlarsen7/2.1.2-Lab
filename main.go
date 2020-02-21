bb84453be4b463e4f5000aff5cff2a198c86961d
nlarsen7 <nlarsen2013@gmail.com>
2020-02-21 05:29:49 +0000

    Lab 2.1.2

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//February 20,2020
+//This takes a 4-digit number and adds each digit together until it is a single digit number
 package main
 
 import "fmt"
@@ -7,17 +10,22 @@ func main() {
 
   fmt.Println("enter a 4-digit number")
   fmt.Scanln(&number)
+  //gets the user's number
 
 for number>999{
   fmt.Println((number/1000) + ((number%1000)/100) + ((number%100)/10) +((number%100)%10))
+  //prints out the number's digits added together
  
   number= (number/1000) + ((number%1000)/100) + ((number%100)/10) +((number%100)%10)
+  //makes the variable that new number
   
   if number<10{
   fmt.Println("your answer is",number)
+  //if the sum is less than 10 it ends here
   
  }else if number>10{
     fmt.Println("Your answer is",number/10 + number%10)
+    //adds the digits again and gives you your answer
   }
   
 }
